8a7411e0fe9438031892a66684e80b76e54dfe29
Ilton Silveira <iltonsilveira91@gmail.com>
2020-02-27 20:43:26 -0300

    Scala Expressions

--- README.md
+++ README.md
@@ -3,12 +3,14 @@ Checkout other resources at: http://github.com/rockthejvm
 
 1. PREFER VAL instead VAR because of the side effect in functional programming
 1. "object" instead "class" private word means that is an instance of a object on runtime instead a class & extends "App" to have: "public static void main(String args[])" 
+1. Use expressions instead loops, __AVOID__ loops are for imperative programming like Java & Python not for functional programming like Scala
+1. 
 
 # Installation
 
 1. [Install JDK 8](https://www.oracle.com/java/technologies/javase/javase-jdk8-downloads.html)
 1. [Install Intellij](https://www.jetbrains.com/idea/download/)
     1. new project
-        1. select __scala__ (left)
+        1. select __Scala__ (left)
         1. select __IDEA__ (right)
      
--- src/lectures/part1basics/Expressions.scala
+++ src/lectures/part1basics/Expressions.scala
@@ -24,7 +24,45 @@ object Expressions extends App {
   val aCondition = true
   val aConditionValue = if(aCondition) 5 else 3 // IF EXPRESSION (works only for expression)
   println(aConditionValue)
+  println(if(aCondition) 5 else 3)
+  println(1+3)
 
+  var i = 0
+  // Loops are Expressions that returns void (Units)
+  var aWhile = while (i<10) {
+    println(i)
+    i+=1
+  } // NEVER WRITE THIS AGAIN (AVOID LOOP)
 
+  // Everything in Scala is an Expression!
+
+  val aWeirdValue = (aVariable = 3) // Unit === void
+  println(aWeirdValue) // () = Unit
+
+  // side effects: println(), whiles, reassigning, return Units
+
+  // Code blocks, are expressions and it VALUE is the last line expression VALUE, in the example aCodeBlocks = "Goodbye"
+  val aCodeBlocks = {
+    val y = 2
+    val z = y+1
+    if(z>2) "Hello" else "Goodbye"
+  }
+//  val anotherV = z +1 ---> Z won't be visible because z was defined inside the
+
+  /*
+    * 1. Diff between "Hello World"(is a String value) and println("Hello World")(is an Expression with SideEffect, it is an Unit)
+    * 2.
+   */
+
+  val someVal = {
+    2 < 3
+  } //someVal will be boolean
+  println(someVal)
+
+  val someOtherVal = {
+    if(someVal) 239 else 986
+    42
+  } //someOtherVal is int
+  println(someOtherVal)
 
 }
